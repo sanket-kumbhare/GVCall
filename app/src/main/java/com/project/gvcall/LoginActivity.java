bbb9ce864737249748fef034f4d9de24ee7be569
Sanket-Kumbhare <sanketkumbhare1511@gmail.com>
2020-12-06 19:16:21 +0530

    Authentication Validation fixed

--- app/src/main/java/com/project/gvcall/LoginActivity.java
+++ app/src/main/java/com/project/gvcall/LoginActivity.java
@@ -51,8 +51,8 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String email, password;
-                email = emailBox.getText().toString();
-                password = passwordBox.getText().toString();
+                email = emailBox.getText().toString().trim();
+                password = passwordBox.getText().toString().trim();
 
                 if (email.isEmpty()) {
                     emailBox.setError("Email is Required");
